f635e452d79ec3851cbbe084039e1933694ebc09
Rory Hellier <rory.hellier@googlemail.com>
2021-01-19 21:37:31 +0000

    Defined initial folder structure, created cake component main file (cakeContainer), added redux folder for cakeActions, cakeTypes and cakeReducer and started hooking everything up

---
+++ src/components/CakeContainer.js
@@ -0,0 +1,20 @@
+import React from 'react';
+
+function CakeContainer() {
+  return (
+    <div>
+      <div>
+        <h2>Number Of Cakes:</h2>
+        <button>Sell Cake</button>
+        <button>Replenish Stock</button>
+      </div>
+      <div>
+        <h2>Number Of Ice-Creams:</h2>
+        <button>Sell Ice-Cream</button>
+        <button>Replenish Stock</button>
+      </div>
+    </div>
+  )
+}
+
+export default CakeContainer;
